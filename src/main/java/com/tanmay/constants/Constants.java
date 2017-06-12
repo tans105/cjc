cd302bfc5b6c424ba7dba0fdc24f4d8efeb36e7b
tans105 <tanmayawasthi105@gmail.com>
2017-06-12 17:50:32 +0530

    CSV reader added

---
+++ src/main/java/com/tanmay/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.tanmay.constants;
+
+/**
+ * @author : tanmay
+ * @created : 12-Jun-2017
+ */
+public class Constants {
+	public static final String CSV_LOCATION = "/home/tanmay/my_projects/GIT/cjc/src/main/resources/test.csv";
+}
--- src/main/java/com/tanmay/controller/MainController.java
+++ src/main/java/com/tanmay/controller/MainController.java
@@ -1,8 +1,16 @@
 package com.tanmay.controller;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+import com.tanmay.constants.Constants;
+
+import au.com.bytecode.opencsv.CSVReader;
+
 /**
  * 
  * @author tanmay
@@ -14,11 +22,29 @@ public class MainController {
 	@GET
 	@Path("/test")
 	public Response test() {
-
 		String output = "Request received";
-
 		return Response.status(200).entity(output).build();
-
 	}
 
+	@SuppressWarnings("resource")
+	@GET
+	@Path("/convert")
+	public Response convert() {
+		String csvFile = Constants.CSV_LOCATION;
+		CSVReader reader = null;
+		String output = "";
+		try {
+			reader = new CSVReader(new FileReader(csvFile));
+			String[] header = reader.readNext();
+			String headers = header[0] + " " + header[1] + " " + header[2];
+			System.out.println(headers);
+			String[] line;
+			while ((line = reader.readNext()) != null) {
+				output += "id= " + line[0] + ", name= " + line[1] + " , country=" + line[2];
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Response.status(200).entity(output).build();
+	}
 }
